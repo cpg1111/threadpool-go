6fac992c302b4724c779dbc47cc747187dc56205
Christian Grabowski <christiang11754@gmail.com>
2017-08-01 23:57:14 -0400

    simplify unit tests

--- thread_test.go
+++ thread_test.go
@@ -2,7 +2,6 @@ package threadpool
 
 import (
 	"context"
-	"sync/atomic"
 	"testing"
 	"time"
 )
@@ -10,23 +9,18 @@ import (
 // TestThreadLifeCycle tests creating, starting, running a function and joining a thread
 func TestThreadLifeCycle(t *testing.T) {
 	var (
-		tasks         int64
-		atomicTestVal int64
+		tasks   int64
+		testVal = make(chan int)
 	)
 	tasksAddr := &tasks
 	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
 	thread := NewOSThread(ctx, cancel, tasksAddr)
 	thread.Start()
 	thread.Exec(func() {
-		time.Sleep(time.Second)
-		atomic.AddInt64(&atomicTestVal, 1)
-		if val := atomic.LoadInt64(&atomicTestVal); val != 1 {
-			t.Errorf("expected atomicTestVal to equal 1, but found %v", val)
-		}
+		testVal <- 1
 	})
-	totalTasks := atomic.LoadInt64(tasksAddr)
-	if totalTasks != 1 {
-		t.Errorf("expected tasks to equal 1, but found %v", totalTasks)
+	if val := <-testVal; val != 1 {
+		t.Errorf("expected tasks to equal 1, but found %v", val)
 	}
 	thread.Join()
 }
